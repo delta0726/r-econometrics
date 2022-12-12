6f11b71d3849c348a76afac85ca78a5df940f2bb
Keido Sumi <delta0726@iuc-net.ne.jp>
2022-12-13 06:56:16 +0900

    Add 6-6

---
+++ book/nishiyama_econometrics/6-6_パネルデータ分析_個別トレンドと年効果.R
@@ -0,0 +1,52 @@
+# ***********************************************************************************************
+# Title   : 計量経済学
+# Chapter : 6 パネルデータ分析
+# Theme   : 実証例6.6 個別トレンドと年効果
+# Date    : 2022/12/13
+# Page    : P241
+# URL     : https://ritsu1997.github.io/r-for-nlas-econometrics/index.html
+# ***********************************************************************************************
+
+
+# ＜目次＞
+# 0 準備
+# 1 固定効果モデルの構築
+# 2 推定結果の確認
+
+
+# 0 準備 ---------------------------------------------------------------
+
+# ライブラリ
+library(tidyverse)
+library(estimatr)
+library(modelsummary)
+library(gridExtra)
+
+
+# データロード
+df <- read_csv("csv/yamaguchi_2.csv")
+
+# データ準備
+df <- df %>% filter(year > 1995 & hh.type == "all")
+
+# データ確認
+df %>% print()
+df %>% glimpse()
+
+
+# 1 固定効果モデルの構築 ------------------------------------------------------
+
+
+# データ確認
+df_model <- df %>% select(pref, emp.rate, cap.rate)
+
+# モデル構築
+model1 <- lm_robust(emp.rate ~ cap.rate + pref + pref:year + factor(year),
+                    data = df, clusters = pref, se_type = "stata")
+
+
+# 2 推定結果の確認 ---------------------------------------------------------------
+
+# 結果確認
+model1 %>% print()
+model1 %>% summary()
